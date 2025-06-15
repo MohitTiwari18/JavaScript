fe082ba655b52078f55518c0a9d1b5ed6b4f4a7f
Mohit Tiwari <mohittiwari9933@gmail.com>
2025-06-15 14:19:21 +0530

    updated js code

---
+++ conditionalStatement.js
@@ -0,0 +1,53 @@
+/* example of condional statements
+1.Agar user login hai to dashboard dikhao.
+2.Agar age 18 se chhoti hai to voting allow mat karo.
+3.Agar cart empty hai to checkout button disable rakho.
+*/
+
+//-------------------- 1 -------------------------------
+
+// //Simple if Statement
+// let age = 20;
+
+// if (age >= 18) {
+//   console.log("You are eligible to vote.");
+// }
+
+// -----------------------2 ---------------------------
+
+// //if...else
+// let isLoggedIn = false;
+
+// if (isLoggedIn) {
+//   console.log("Welcome to your dashboard!");
+// } else {
+//   console.log("Please login to continue.");
+// }
+
+//--------------------- 3 ---------------------------
+
+// //if...else if...else
+// let marks = 75;
+
+// if (marks >= 90) {
+//   console.log("Grade: A");
+// } else if (marks >= 75) {
+//   console.log("Grade: B");
+// } else if (marks >= 60) {
+//   console.log("Grade: C");
+// } else {
+//   console.log("Grade: Fail");
+// }
+
+//---------------------------- 4 ---------------------
+
+// //practice question - Q. Write a code that checks if a number is positive, negative, or zero:
+// let num = 0;
+
+// if (num > 0) {
+//   console.log("Positive");
+// } else if (num < 0) {
+//   console.log("Negative");
+// } else {
+//   console.log("Zero");
+// }
---
+++ loops.js
@@ -0,0 +1,39 @@
+// | Loop Type         | Use Kab Karte Hain                                                       |
+// | ----------------- | ------------------------------------------------------------------------ |
+// | `for` loop        | Jab repeat karne ki **limit pehle se pata ho**                           |
+// | `while` loop      | Jab repeat karne ki limit **runtime pe decide ho**                       |
+// | `do...while` loop | Jab **kam se kam 1 baar code run karna hi ho**, chahe condition false ho |
+
+//-------------------- 1 -------------------------
+
+// //for loop
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Hello " + i);
+// }
+
+// ------------------ 2 ------------------------
+
+// //while loop
+// let i = 1;
+// while (i <= 3) {
+//   console.log("Number is: " + i);
+//   i++;
+// }
+
+// ----------------- 3 --------------------------
+
+// //do while loop
+// let i = 5;
+
+// do {
+//   console.log("Value: " + i);
+//   i++;
+// } while (i < 5);
+
+//-------------------4-----------------------
+
+// //practice question. Q. Print numbers from 10 to 1 using for loop:
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
